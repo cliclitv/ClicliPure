48e44a62299d243f608a4472cbad7b8110323389
132yse <1533540012@qq.com>
2020-01-11 09:31:43 +0800

    格式化

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hello world!</Text>
+      <Text>hello worl</Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -14,6 +14,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})
