4d9c007771feb9da62f0d5cfea3ad5e1799967ac
132yse <1533540012@qq.com>
2020-01-12 10:03:58 +0800

    重新遍历

--- component/Anime/Anime.js
+++ component/Anime/Anime.js
@@ -6,14 +6,33 @@ import ListView from '../ListView/ListView'
 
 export default function Home(props) {
   const [post, setPost] = useState([])
+  const map = {
+    0: '周日',
+    1: '周一',
+    2: '周二',
+    3: '周三',
+    4: '周四',
+    5: '周五',
+    6: '周六'
+  }
   useEffect(() => {
-    getPost('新番', '', 1, 10).then(res => setPost(res.posts))
+    getPost('新番', '', 1, 10).then(res => {
+      let ret = []
+      for (const k in map) ret.push({ title: map[k], data: [] })
+      res.posts.forEach(item => {
+        const time = item.time.replace(/\-/g, '/')
+        let day = new Date(time).getDay().toString()
+        let data = ret[day].data
+        data.push(item)
+      })
+      setPost(ret)
+    })
   }, [])
   return (
     <View style={s.container}>
       <StatusBar barStyle={'dark-content'} translucent={true} backgroundColor='transparent' />
       <Text style={s.title}> UGC </Text>
-      <ListView data={post} />
+      <ListView sections={post} />
     </View>
   )
 }
