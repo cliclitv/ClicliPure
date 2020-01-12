f3290599688f17613553edbc3efe6fff28304291
132yse <1533540012@qq.com>
2020-01-12 18:59:45 +0800

    修复video背景bug

--- widget/Player/Player.js
+++ widget/Player/Player.js
@@ -1,13 +1,10 @@
 import React from 'react'
 import { Video } from 'expo-av'
 import VideoPlayer from 'expo-video-player'
-import { Dimensions } from 'react-native'
+import { View } from 'react-native'
 
 export default function Player({ url, type }) {
-  const { width } = Dimensions.get('window')
-  const height = (width * 9) / 16
   const m = type === 'hls' ? 'm3u8' : type
-  console.log(url,m)
   const a = (
     <Video
       source={{ uri: url, overrideFileExtensionAndroid: m }}
@@ -17,22 +14,22 @@ export default function Player({ url, type }) {
       resizeMode='cover'
       shouldPlay
       isLooping
-      style={{ width, height }}
+      style={{ width: '100%', height: '100%' }}
     />
   )
-  const b = (
-    <VideoPlayer
-      videoProps={{
-        shouldPlay: true,
-        resizeMode: Video.RESIZE_MODE_CONTAIN,
-        source: {
-          uri: url
-        }
-      }}
-      inFullscreen={true}
-      sliderColor='#a10cff'
-      height={230}
-    />
-  )
+  // const b = (
+  //   <VideoPlayer
+  //     videoProps={{
+  //       shouldPlay: true,
+  //       resizeMode: Video.RESIZE_MODE_CONTAIN,
+  //       source: {
+  //         uri: url
+  //       }
+  //     }}
+  //     inFullscreen={true}
+  //     sliderColor='#a10cff'
+  //     height={230}
+  //   />
+  // )
   return a
 }
